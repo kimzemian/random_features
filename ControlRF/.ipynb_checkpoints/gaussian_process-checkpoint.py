df2325aab66b00f54589fa7883a6fa864abc946f
Kimia Kazemian <kimia@Kimias-MacBook-Pro.local>
2022-11-10 18:28:13 -0500

    cleaning up

--- ControlRF/.ipynb_checkpoints/gaussian_process-checkpoint.py
+++
@@ -1,9 +0,0 @@
-class GP:
-    def __init__(self, x_train, y_train, z_train, x_test, y_test, z_test):
-        self.x_train = x_train
-        self.y_train = y_train
-        self.z_train = z_train
-
-
-
-
