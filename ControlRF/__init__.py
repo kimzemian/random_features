a502babc88284609473225d31e003352fbd71eb0
Kimia Kazemian <kimia@Kimias-MacBook-Pro.local>
2022-12-15 00:04:27 -0500

    both controllers work with inverted pendulum

--- ControlRF/__init__.py
+++ ControlRF/__init__.py
@@ -3,5 +3,6 @@ from .kernel_adp_gp import KernelADPGP
 from .rf_adp_gp import RandomFeaturesADPGP
 from .kernel_gp import KernelGP
 from .rf_gp import RandomFeaturesGP
-from .adp_controller import ADPGPController
+from .gp_controller import GPController
+from .adp_controller import ADPController
 from .ad_controller import ADController
--- ControlRF/kernel_gp.py
+++ ControlRF/kernel_gp.py
@@ -13,58 +13,67 @@ class KernelGP(GaussianProcess):
         self.inv_kernel = []
         self.k_vec = []
 
-    def _compute_kernel_helper(self, x_test, test=False):
-        train_k = np.exp(-np.sum(np.square(self.x_train), axis=2, keepdims=True)/(2 * self.sigma ** 2)) #(n,1)
-        if test:
-            test_k = np.exp(-np.sum(np.square(x_test), axis=2, keepdims=True)/(2 * self.sigma ** 2)) #(n_t,1)
+    def rbf(self, x, axis=1, keepdims=False):
+        return np.exp(-np.sum(np.square(x), axis=axis, keepdims=keepdims)/(2 * self.sigma ** 2))
+
+    def _compute_kernel_helper(self, x_test=None):
+        train_k = self.rbf(self.x_train, keepdims=True) #(n,1)
+        if x_test is not None:
+            test_k = self.rbf(x_test, keepdims=True) #(n_t,1)
         else:
             test_k = train_k
+            x_test = self.x_train
         k_mat = train_k @ test_k.T #(n,n_t)
-        x_dif = self.x_train.reshape((self.n,1,self.d)) - x_test #(n,n_t,d)
-        diag_k = np.exp(-np.sum(np.square(x_dif), axis=2)/(2 * self.sigma ** 2)) #(n,n_t)
+        x_diff = self.x_train.reshape((self.n,1,self.d)) - x_test #(n,n_t,d)
+        diag_k = self.rbf(x_diff, axis=2) #(n,n_t)
         return k_mat, diag_k
 
-    def _compute_kernel(self, k_mat, diag_k, y_test):
-        ys = np.sum(self.y_train, 1, keepdims=True) @ np.sum(y_test, 1) #(n,n_t)
+    def _compute_kernel(self, x_test=None, y_test=None):
+        k_mat, diag_k = self._compute_kernel_helper(x_test)
+        ytr_sum = np.sum(self.y_train, axis=1, keepdims=True)
+        if y_test is not None:
+            ys =  ytr_sum @ np.sum(y_test, axis=1, keepdim=True).T #(n,n_t)
+        else:
+            ys = ytr_sum @ ytr_sum.T
+            y_test = self.y_train
         diag_diff = (self.y_train @ y_test.T) * (diag_k - k_mat) #(n,n_t)
         return k_mat * ys + diag_diff #(n,n_t)
 
     def _compute_entries(self, x_test, y_test):
         ys = np.square(np.sum(y_test,1)) #(n_t)
-        test_k = np.exp(-np.sum(np.square(x_test), axis=2)/(2 * self.sigma ** 2)) #(n_t)
-        k_diag = np.einsum('ij,ji->i',test_k, test_k.T) #(n_t)
+        test_k = self.rbf(x_test) #(n_t)
+        k_diag = np.square(test_k) #(n_t)
         diff = np.einsum('ij,ji->i', y_test, y_test.T) * (1 - k_diag) #(n_t)
         return k_diag * ys + diff #(n_t)
+    
+    def _compute_k_h(self, x_test): #n_t=1
+        k_mat, diag_k = self._compute_kernel_helper(x_test) #(n,n_t)
+        diag_diff = (diag_k- k_mat) * self.y_train #(n,m+1)
+        self.k_h = k_mat * np.sum(self.y_train, 1, keepdims=True) + diag_diff #(n,m+1)
+        return
 
     def train(self): 
         # kernel training
-        self.kernel = self._compute_kernel(self.x_train, self.y_train)
+        self.kernel = self._compute_kernel()
         self.inv_kernel = la.inv(self.kernel + self.sigma_n**2 * np.identity(self.n)) #(n,n)          
 
     def test(self, x_test, y_test):
         # c = (K+sigma^2I)^{-1}K(x)
         self.n_t = len(x_test)
-        self.k_vec = self._compute_kernel(x_test, y_test, test=True) #(n,n_t)
+        self.k_vec = self._compute_kernel(x_test, y_test) #(n,n_t)
         return  self.z_train @ self.inv_kernel @ self.k_vec #n_t
     
     def sigma(self, x_test, y_test): #n_t=1
         return self._compute_entries(x_test, y_test) - \
-            np.einsum('ij,jk,ki->i', self.k_vec.T, self.inv_ckernel, self.k_vec)
+            np.einsum('ij,jk,ki->i', self.k_vec.T, self.inv_kernel, self.k_vec)
     
-    def _compute_k_h(self, x_test): #n_t=1
-        k_mat, diag_k = self._compute_kernel_helper(x_test, test=True) #(n,n_t)
-        diag_diff = (diag_k- k_mat) * self.y_train #(n,m+1)
-        self.k_h = k_mat * np.sum(self.y_train, 1, keepdims=True) + diag_diff #n,n_t * n,1?????
-        return
-
-
     def mean_var(self, x_test): #n_t=1
         self._compute_k_h(x_test)
-        meanvar = self.z_train @ self.inv_ckernel @ self.k_h #m+1
+        meanvar = self.z_train @ self.inv_kernel @ self.k_h #m+1
         return meanvar.T #y @ meanvar
     
     def sigma_var(self): #n_t=1
-        sigmavar = sqrtm(np.identity(self.m+1)-self.k_h.T@self.inv_ckernel@self.k_h) #(m+1,m+1)
+        sigmavar = sqrtm(np.identity(self.m+1) - self.k_h.T @ self.inv_kernel @ self.k_h) #(m+1,m+1)
         # norm(y @ sigmavar) 
         return sigmavar
 
